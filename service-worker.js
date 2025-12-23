8f91b841dce8872dff4bb65c841ddf187aa98352
buecki2 <simon.ruemmler.94@gmail.com>
2025-12-23 13:34:48 +0100

    Added cache logic to service worker

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hello-pwa-v2';
+const CACHE_NAME = 'hello-pwa-v3';
 const FILES_TO_CACHE = ['./', './index.html', './main.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', evt => {
@@ -12,4 +12,17 @@ self.addEventListener('fetch', evt => {
   evt.respondWith(
     caches.match(evt.request).then(cached => cached || fetch(evt.request))
   );
+});
+
+// Activate event (delete old caches)
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME) // keep only current cache
+            .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim(); // take control of all clients immediately
 });
